abfb054b3e3bb2022319d45f7657e32be77a76f3
Zver_ID <helix999@mail.ru>
2025-10-28 22:29:37 +0400

    Добавил модель команды

---
+++ Server/CommonModels/Team.cs
@@ -0,0 +1,17 @@
+﻿namespace CommonModels;
+
+/// <summary>
+/// Команда.
+/// </summary>
+public class Team
+{
+  /// <summary>
+  /// ID команды.
+  /// </summary>
+  public int Id { get; set; }
+  
+  /// <summary>
+  /// Имя команды.
+  /// </summary>
+  public string Name { get; set; }
+}
