ea55a99cd899dfc9381fcfbcbde8ee1ae568b13d
Zver_ID <helix999@mail.ru>
2025-11-03 21:28:54 +0400

    Добавил классы DTO

--- Server/WebAPI/DTO/MetricDTO.cs
+++ Server/WebAPI/DTO/MetricDTO.cs
@@ -1,6 +1,34 @@
+using System;
+
 namespace WebAPI.DTO;
 
+/// <summary>
+/// DTO метрики.
+/// </summary>
 public class MetricDTO
 {
+  /// <summary>
+  /// ID метрики.
+  /// </summary>
+  public int Id { get; set; }
   
+  /// <summary>
+  /// Дата метрики.
+  /// </summary>
+  public DateTime Date { get; set; }
+  
+  /// <summary>
+  /// Команда, которой принадлежит метрика.
+  /// </summary>
+  public string Team { get; set; }
+  
+  /// <summary>
+  /// Тип метрики.
+  /// </summary>
+  public string MetricType { get; set; }
+  
+  /// <summary>
+  /// Значение метрики.
+  /// </summary>
+  public float Value { get; set; }
 }
---
+++ Server/WebAPI/DTO/TeamDTO.cs
@@ -0,0 +1,18 @@
+namespace WebAPI.DTO;
+
+/// <summary>
+/// DTO комманды.
+/// </summary>
+public class TeamDTO
+{
+  /// <summary>
+  /// ID команды.
+  /// </summary>
+  public int Id { get; set; }
+  
+  /// <summary>
+  /// Имя команды.
+  /// </summary>
+  public string Name { get; set; }
+  
+}
