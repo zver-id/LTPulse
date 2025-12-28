4b0975657c00f689b2d144cf9cc89eadd15ac44a
Zver_ID <helix999@mail.ru>
2025-12-28 13:56:19 +0400

    Добавил ограничение на 3 столбца в таблице метрик
    Нужно для того чтобы не создать одну метрику несколько раз

--- Server/DBCore/Overrides/MetricOverride.cs
+++ Server/DBCore/Overrides/MetricOverride.cs
@@ -10,12 +10,18 @@ public class MetricOverride : IAutoMappingOverride<Metric>
   public void Override(AutoMapping<Metric> mapping)
   {
     //TODO нужно добавить в индекс столбцы команды и метрики возможно будет быстрее
-    mapping.Map(x => x.Date)
-      .Index("Ix_MetricDate");
-    mapping.Map(x=>x.Date).Not.Nullable();
-    mapping.References(x=>x.MetricType).Not.Nullable();
-    mapping.References(x => x.Team).Not.Nullable();
     
-    //TODO нужно ограничение на три столбца
+    mapping.Map(x=>x.Date)
+      .Not.Nullable()
+      .Index("Ix_MetricDate")
+      .UniqueKey("Key_Date_MetricType_Team");
+    
+    mapping.References(x=>x.MetricType)
+      .Not.Nullable()
+      .UniqueKey("Key_Date_MetricType_Team");
+    
+    mapping.References(x => x.Team)
+      .Not.Nullable()
+      .UniqueKey("Key_Date_MetricType_Team");
   }
 }
