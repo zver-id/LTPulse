d16e080779077cd72a79a8f5c92343b53df6ad6d
Zver_ID <helix999@mail.ru>
2026-04-01 21:15:18 +0400

    Поправил возвращаемое значение в контроллере обращений

--- Server/WebAPI/Controllers/TicketsController.cs
+++ Server/WebAPI/Controllers/TicketsController.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Application;
+using AutoMapper;
 using CommonModels.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using WebAPI.DTO;
 
 namespace WebAPI.Controllers;
 
@@ -11,14 +14,16 @@ namespace WebAPI.Controllers;
 public class TicketsController : ControllerBase
 {
   private readonly IRepository repository;
+  private readonly IMapper mapper;
   
   [HttpGet]
-  public async Task<IActionResult> GetTickets(int teamId, DateTime date, string ticketType)
+  public async Task<ActionResult<List<TicketDTO>>> GetTickets(int teamId, DateTime date, string ticketType)
   {
     try
     {
       var service = new TicketService(this.repository);
-      var result = await service.GetTickets(teamId, date, ticketType);
+      var ticketList = await service.GetTickets(teamId, date, ticketType);
+      var result = this.mapper.Map<List<TicketDTO>>(ticketList);
       return this.Ok(result);
     }
     catch (ArgumentException ex)
@@ -27,8 +32,9 @@ public class TicketsController : ControllerBase
     }
   }
 
-  public TicketsController(IRepository repository)
+  public TicketsController(IRepository repository, IMapper mapper)
   {
     this.repository = repository;
+    this.mapper = mapper;
   }
 }
