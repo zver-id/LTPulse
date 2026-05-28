badd68757172dd588618560e33a30146384a0c24
zver-id <helix999@mail.ru>
2026-05-28 13:41:23 +0400

    Пустая страница настройки команды

--- client/src/components/router/Router.tsx
+++ client/src/components/router/Router.tsx
@@ -1,10 +1,12 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Main from "../mainScreen/MainScreen.tsx";
+import TeamsSettings from "../teamsSettings/teamsSettings.tsx";
 
 const Router = () => {
   return <BrowserRouter>
     <Routes>
       <Route element={<Main/>} path={"/"}/>
+      <Route element={<TeamsSettings/>} path={"/teams"}/>
     </Routes>
   </BrowserRouter>
 }
---
+++ client/src/components/teamsSettings/teamsSettings.tsx
@@ -0,0 +1,29 @@
+﻿import {Select} from "antd";
+import styles from "./TeamsSettings.module.css";
+import {useLocalStorageState} from "../../storage/useLocalStorageState.ts";
+import {useGetAllTeamsQuery} from "../../storage/services/teams-api.ts";
+
+function TeamsSettings(){
+  const [team, setTeam] = useLocalStorageState("team", 1)
+  const {data} = useGetAllTeamsQuery()
+
+  return <>
+    <header>
+      <h1 className={styles.siteTitle}>Настройки команд </h1>
+      <Select
+        className={styles.select}
+        defaultValue={{value: team.id, label: team.name}}
+        onChange={setTeam}
+        options={data?.map(team => ({
+          value: team.id,
+          label: team.name
+        }))}
+      />
+    </header>
+    <main>
+
+    </main>
+  </>
+}
+
+export default TeamsSettings
